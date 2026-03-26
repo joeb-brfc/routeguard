ba4980991f4f3c08bee1573affc9527db2c4f805
joeb_brfc <joseph_bird93@hotmail.co.uk>
2026-03-26 11:44:36 +0000

    Add URL pattern for assignment list page

--- planner/urls.py
+++ planner/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path("", views.home, name="home"),
+    path("assignments/", views.assignment_list, name="assignment_list"),
 ]
