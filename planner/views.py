5c8d223049fe271e3a88e9b2a294dedbab897fd3
joeb_brfc <joseph_bird93@hotmail.co.uk>
2026-03-26 11:43:42 +0000

    Add assignment list view to display assignment records

--- planner/views.py
+++ planner/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from .models import Assignment
+
 
 def home(request):
     return render(request, "planner/home.html")
+
+
+def assignment_list(request):
+    assignments = Assignment.objects.all()
+    return render(
+        request,
+        "planner/assignment_list.html",
+        {"assignments": assignments},
+    )
