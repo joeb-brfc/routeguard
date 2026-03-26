7d304e51a61a3fb1e1852feaaaf0385185029a98
joeb_brfc <joseph_bird93@hotmail.co.uk>
2026-03-26 12:54:51 +0000

    Add URL pattern for route list page

--- planner/urls.py
+++ planner/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("assignments/", views.assignment_list, name="assignment_list"),
     path("drivers/", views.driver_list, name="driver_list"),
+    path("routes/", views.route_list, name="route_list"),
 ]
