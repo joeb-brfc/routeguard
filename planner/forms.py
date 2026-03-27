6749037f043c032e01a94c1c9428130e9e4bea3b
joeb_brfc <joseph_bird93@hotmail.co.uk>
2026-03-27 11:09:55 +0000

    Create DriverForm model form for driver creation

---
+++ planner/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Driver
+
+
+class DriverForm(forms.ModelForm):
+    class Meta:
+        model = Driver
+        fields = ["name", "employee_id", "active"]
