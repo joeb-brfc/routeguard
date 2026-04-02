088cbeafeed2354dc87af97ddb2ee14a3e079ffb
joeb_brfc <joseph_bird93@hotmail.co.uk>
2026-04-02 15:32:57 +0100

    Add Django authentication URL patterns for login and logout

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('planner.urls')),  # 👈 THIS LINE
+    path("admin/", admin.site.urls),
+    path("", include("planner.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
